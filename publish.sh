4592e31b9d98a56180d75f99bd656301c28ac011
Krusty/Benediction <krusty.benediction@gmail.com>
2023-10-04 15:20:46 +0200

    fix typo in publis?sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-cargo publish -p hxcfeadaptor && \
-	cargo publish -p hxcfeadaptor-sys && \
+cargo publish -p hxcadaptor && \
+	cargo publish -p hxcadaptor-sys && \
 	cargo publish -p hxcfe-sys && \
 	cargo publish -p hxcfe
