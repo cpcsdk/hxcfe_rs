18d2b3da5aa8c387d4ad3f4faf58ef3da0aedecd
Krusty/Benediction <krusty.benediction@gmail.com>
2023-10-04 15:16:58 +0200

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-cargo publish -p hxcfeadaptor/
-cargo publish -p hxcfeadaptor-sys/
-cargo publish -p hxcfe-sys/
-cargo publish -p hxcfe
+cargo publish -p hxcfeadaptor && \
+	cargo publish -p hxcfeadaptor-sys && \
+	cargo publish -p hxcfe-sys && \
+	cargo publish -p hxcfe
