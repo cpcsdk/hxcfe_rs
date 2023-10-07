9c79d556227f5059981e2d4260f229e7e8bb14c3
Krusty/Benediction <krusty.benediction@gmail.com>
2023-10-07 17:10:31 +0200

    Make public dependenceis of hxcfe-sys

--- hxcfe-sys/src/lib.rs
+++ hxcfe-sys/src/lib.rs
@@ -4,6 +4,9 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+pub use hxcadaptor;
+pub use hxcadaptor::hxcadaptor_sys;
+
 #[cfg(test)]
 mod test {
     use crate::{hxcfe_deinit, hxcfe_init};
